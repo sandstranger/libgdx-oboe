a28a1d69ae946ffd34e6ea12d98c11da29c96c58
Evgeniy Mineev <e.mineev@crazypanda.ru>
2024-12-14 21:27:19 +0400

    update libgdx

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -5,7 +5,10 @@ plugins {
 }
 
 dependencies {
-    implementation("com.badlogicgames.gdx:gdx-backend-android:1.13.0")
+    implementation ("com.badlogicgames.gdx:gdx-backend-android:1.13.0")
+    implementation  ("com.badlogicgames.gdx:gdx:1.13.0")
+    implementation  ("com.badlogicgames.gdx:gdx-box2d:1.13.0")
+    implementation  ("com.badlogicgames.gdx:gdx-freetype:1.13.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
 }
 
